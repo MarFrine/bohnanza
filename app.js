10d869f5da012a104619e69177d9ba478319eb52
MarFrine <81744843+MarFrine@users.noreply.github.com>
2022-10-02 13:27:38 +0200

    port changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const socket = require("socket.io")
 const fs = require("fs")
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const server = app.listen(port, (req, res)=>{
     console.log("Server on port: " + port)
 })
